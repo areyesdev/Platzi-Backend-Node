404267f390353c28cb03a9294faa54e25453b3db
Andres Reyes <ar97yf@gmail.com>
2020-10-28 22:22:01 -0500

    Cache-Control and DockerFile

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -6,6 +6,8 @@ const {
     updateMovieSchema
 } = require('../utils/schemas/movies');
 const validationHandler = require('../utils/middleware/validationHandler');
+const cacheResponse = require('../utils/cacheResponse');
+const {FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS} = require('../utils/time')
 
 
 function moviesApi(app) {
@@ -15,6 +17,7 @@ function moviesApi(app) {
     const moviesService = new MoviesService();
 
     router.get("/", async function (req, res, next) {
+        cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
         const { tags } = req.query;
         try {
             const movies = await moviesService.getMovies({ tags });
@@ -27,6 +30,7 @@ function moviesApi(app) {
         }
     });
     router.get("/:movieId", validationHandler({ movieId: movieIdSchema }, 'params'), async function (req, res, next) {
+        cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
         const { movieId } = req.params;
         try {
             const movies = await moviesService.getMovie({ movieId })
